db6a45822058d00e743f66b9c0e4a2233da871f1
Ioannis (Yiannis) Doulgerakis <doulgerakisyiannis@yahoo.com>
2025-06-01 23:44:32 +1000

    Fix Firebase Configuration

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,9 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-// TODO: Add SDKs for Firebase products that I want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -12,11 +9,16 @@ const firebaseConfig = {
     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  };
+};
 
 // Initialize Firebase and Firestore
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+// Connect to Firestore emulator if running locally
+if (window.location.hostname === "localhost") {
+    connectFirestoreEmulator(db, 'localhost', 8080);
+}
 
 export { app };
 export { db };
